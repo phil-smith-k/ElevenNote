6b6b964e2da1beb330f503bc850cae208b52c72b
Philip Smith <philip.smith.k@gmail.com>
2020-02-27 14:00:29 -0500

    Get Note By Id Method added

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -33,7 +33,26 @@ namespace ElevenNote.Services
                 return ctx.SaveChanges() == 1;
             }
         }
+        public NoteDetail GetNoteById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == id && e.OwnerId == _userId);
 
+                return
+                    new NoteDetail
+                    {
+                        NoteId = entity.NoteId,
+                        Title = entity.Title,
+                        Content = entity.Content,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc
+                    };
+            }
+        }
         public IEnumerable<NoteListItem> GetNotes()
         {
             using (var ctx = new ApplicationDbContext())
---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,51 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    public class NoteController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            NoteService noteService = CreateNoteService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            NoteService noteService = CreateNoteService();
+            var note = noteService.GetNoteById(id);
+            return Ok(note);
+        }
+
+        public IHttpActionResult Post(NoteCreate note)
+        {
+            if (!this.ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.CreateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+        
+        
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(this.User.Identity.GetUserId());
+            var noteService = new NoteService(userId);
+            return noteService;
+        }
+    }
+}
