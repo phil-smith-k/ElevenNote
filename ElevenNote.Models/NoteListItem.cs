00b12122f545a55cb734e561e5238399467702db
Philip Smith <philip.smith.k@gmail.com>
2020-02-27 12:46:51 -0500

    added Get All Notes, CreateNotes methods)

---
+++ ElevenNote.Models/NoteListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteListItem
+    {
+        [Key]
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
